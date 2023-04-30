e9c6542ed0f10160059e95e4cfe987697c63378d
joshuanieh <b07502027@ntu.edu.tw>
2023-04-30 12:39:38 +0800

    rank K

--- G.v
+++ G.v
@@ -6,9 +6,9 @@ module GSIM ( clk, reset, in_en, b_in, out_valid, x_out);
     input   in_en;
     output  out_valid;
     input   [15:0]  b_in;
-    output  [31:0]  x_out;
+    output reg [31:0]  x_out;
 
-    parameter RUN = 300; //after this run number, the out_valid will be pulled up
+    parameter RUN = 100; //after this run number, the out_valid will be pulled up
 
     wire [32-1:0] x;
     wire [32-1:0] x1, x2, x3, x4, x5, x6;
@@ -18,6 +18,10 @@ module GSIM ( clk, reset, in_en, b_in, out_valid, x_out);
     reg  [9-1:0]  run_count_r;
     reg  [9-1:0]  run_count_w;
     wire          start;
+    reg  [32-1:0] x_stored_r[1:16];
+    reg  [32-1:0] x_stored_w[1:16];
+
+    integer       i;
 
     register_file register_file (
         .clk_in(clk),
@@ -74,8 +78,63 @@ module GSIM ( clk, reset, in_en, b_in, out_valid, x_out);
         run_count_r <= run_count_w;
     end
 
-    assign x_out     = x; //just output the x computed
-    assign out_valid = (start && ((run_count_r == RUN && cycle_count_r >= 4'd1) || (run_count_r == RUN + 1 && cycle_count_r == 4'd0))) ? 1'b1 : 1'b0; //pulled up when run_count_r achieve RUN
+    // assign x_out     = x; //just output the x computed
+    always @(*) begin
+        for (i = 1; i < 16+1; i = i + 1) begin
+            x_stored_w[i] = x_stored_r[i];
+        end
+        if (start && ((run_count_r == RUN && cycle_count_r >= 4'd1) || (run_count_r == RUN + 1 && cycle_count_r == 4'd0))) begin
+            x_stored_w[16] = x;
+            for (i = 1; i < 16; i = i + 1) begin
+                x_stored_w[i] = x_stored_r[i+1];
+            end
+        end
+    end
+
+    always @(posedge clk) begin
+        for (i = 1; i < 16+1; i = i + 1) begin
+            x_stored_r[i] <= x_stored_w[i];
+        end
+    end
+
+    always @(*) begin
+        case (cycle_count_r)//1 5 9 13 2 6 10 14 3 7 11 15 4 8 12 16
+            4'd0:
+                x_out =  x_stored_r[1];
+            4'd1:
+                x_out =  x_stored_r[5];
+            4'd2:
+                x_out =  x_stored_r[9];
+            4'd3:
+                x_out =  x_stored_r[13];
+            4'd4:
+                x_out =  x_stored_r[2];
+            4'd5:
+                x_out =  x_stored_r[6];
+            4'd6:
+                x_out =  x_stored_r[10];
+            4'd7:
+                x_out =  x_stored_r[14];
+            4'd8:
+                x_out =  x_stored_r[3];
+            4'd9:
+                x_out =  x_stored_r[7];
+            4'd10:
+                x_out =  x_stored_r[11];
+            4'd11:
+                x_out =  x_stored_r[15];
+            4'd12:
+                x_out =  x_stored_r[4];
+            4'd13:
+                x_out =  x_stored_r[8];
+            4'd14:
+                x_out =  x_stored_r[12];
+            4'd15:
+                x_out =  x_stored_r[16];
+        endcase
+    end
+
+    assign out_valid = (start && (run_count_r == RUN + 2)) ? 1'b1 : 1'b0; //pulled up when run_count_r achieve RUN
 
 endmodule
 
@@ -96,48 +155,66 @@ module register_file (
     start_out
 );
 
-    input           clk_in, rst_in, en_in;
-    input  [16-1:0] b_in;
-    input  [32-1:0] x_in;
-    output [16-1:0] b_out;
-    output [32-1:0] x1_out, x2_out, x3_out, x4_out, x5_out, x6_out;
-    output          start_out;
+    input               clk_in, rst_in, en_in;
+    input      [16-1:0] b_in;
+    input      [32-1:0] x_in;
+    output     [16-1:0] b_out;
+    output reg [32-1:0] x1_out, x2_out, x3_out, x4_out, x5_out, x6_out;
+    output              start_out;
 
-    reg    [16-1:0] b_r[0:16-1];
-    reg    [16-1:0] b_w[0:16-1];
+    reg        [16-1:0] b_r[1:16];
+    reg        [16-1:0] b_w[1:16];
     
-    reg    [32-1:0] x_r[0:16-1];
-    reg    [32-1:0] x_w[0:16-1];
+    reg        [32-1:0] x_r[1:16]; //1 5 9 13 2 6 10 14 3 7 11 15 4 8 12 16
+    reg        [32-1:0] x_w[1:16];
 
-    reg    [4-1:0] count_r;
-    reg    [4-1:0] count_w;
+    reg        [4-1:0]  count_r;
+    reg        [4-1:0]  count_w;
 
-    reg            start_r;
-    reg            start_w;
+    reg                 start_r;
+    reg                 start_w;
 
-    reg            delay_start_r;
-    reg            delay_start_w;
+    reg                 delay_start_r;
+    reg                 delay_start_w;
 
-    integer i;
+    integer             i;
 
     //Handle b_r, when en_in == 1'b1, read b_in and shift to lower index; otherwise, shift circularly
     always @(*) begin
         if (en_in == 1'b1) begin
-            b_w[15] = b_in;
-            for (i = 0; i < 16 - 1; i = i + 1) begin
+            b_w[16] = b_in;
+            for (i = 1; i < 16; i = i + 1) begin
                 b_w[i] = b_r[i+1];
             end
         end
+        else if (start_r == 1'b1 && delay_start_r == 1'b0) begin //b_r stored in this order 1 5 9 13 2 6 10 14 3 7 11 15 4 8 12 16
+            b_w[1] = b_r[5];
+            b_w[2] = b_r[9];
+            b_w[3] = b_r[13];
+            b_w[4] = b_r[2];
+            b_w[4] = b_r[6];
+            b_w[6] = b_r[10];
+            b_w[7] = b_r[14];
+            b_w[8] = b_r[3];
+            b_w[9] = b_r[7];
+            b_w[10] = b_r[11];
+            b_w[11] = b_r[15];
+            b_w[12] = b_r[4];
+            b_w[13] = b_r[8];
+            b_w[14] = b_r[12];
+            b_w[15] = b_r[16];
+            b_w[16] = b_r[1];
+        end
         else begin
-            b_w[15] = b_r[0];
-            for (i = 0; i < 16 - 1; i = i + 1) begin
+            b_w[16] = b_r[1];
+            for (i = 1; i < 16; i = i + 1) begin
                 b_w[i] = b_r[i+1];
             end
         end
     end
 
     always @(posedge clk_in) begin
-        for (i = 0; i < 16; i = i + 1) begin
+        for (i = 1; i < 16+1; i = i + 1) begin
             b_r[i] <= b_w[i];
         end
     end
@@ -145,20 +222,20 @@ module register_file (
     //Handle x_r, when reset is high, reset to initial values; else store their values until start_r is high, note that the delay_start_r is due to the pipeline of computational unit.
     always @(*) begin
         if (delay_start_r == 1'b1) begin //shift and store x_in to x[14]
-            x_w[15] = x_r[0];
-            x_w[14] = x_in;
-            for (i = 0; i < 16 - 2; i = i + 1) begin
+            x_w[16] = x_r[1];
+            x_w[15] = x_in;
+            for (i = 1; i < 16 - 1; i = i + 1) begin
                 x_w[i] = x_r[i+1];
             end
         end
         else if (start_r == 1'b1) begin //shift but not store x_in
-            x_w[15] = x_r[0];
-            for (i = 0; i < 16 - 1; i = i + 1) begin
+            x_w[16] = x_r[1];
+            for (i = 1; i < 16; i = i + 1) begin
                 x_w[i] = x_r[i+1];
             end
         end
         else begin
-            for (i = 0; i < 16; i = i + 1) begin
+            for (i = 1; i < 16+1; i = i + 1) begin
                 x_w[i] = x_r[i];
             end
         end
@@ -166,7 +243,6 @@ module register_file (
 
     always @(posedge clk_in or posedge rst_in) begin
         if (rst_in == 1'b1) begin //Run initial_x_value_generator.py for random initial values
-            x_r[0] <= 32'd0;
             x_r[1] <= 32'd0;
             x_r[2] <= 32'd0;
             x_r[3] <= 32'd0;
@@ -182,9 +258,10 @@ module register_file (
             x_r[13] <= 32'd0;
             x_r[14] <= 32'd0;
             x_r[15] <= 32'd0;
+            x_r[16] <= 32'd0;
         end
         else begin
-            for (i = 0; i < 16; i = i + 1) begin
+            for (i = 1; i < 16+1; i = i + 1) begin
                 x_r[i] <= x_w[i];
             end
         end
@@ -229,13 +306,139 @@ module register_file (
             delay_start_r <= delay_start_w;
     end
 
-    assign b_out     = b_r[0];                               //always output b_r[0] and shift from higher index
-    assign x1_out    = (count_r == 4'd15) ? 32'd0 : x_r[1];  //see ppt P.4 for this implementation
-    assign x2_out    = (count_r == 4'd0)  ? 32'd0 : x_r[15]; //see ppt P.4 for this implementation
-    assign x3_out    = (count_r >= 4'd14) ? 32'd0 : x_r[2];  //see ppt P.4 for this implementation
-    assign x4_out    = (count_r <= 4'd1)  ? 32'd0 : x_r[14]; //see ppt P.4 for this implementation
-    assign x5_out    = (count_r >= 4'd13) ? 32'd0 : x_r[3];  //see ppt P.4 for this implementation
-    assign x6_out    = (count_r <= 4'd2)  ? 32'd0 : x_r[13]; //see ppt P.4 for this implementation
+    assign b_out     = b_r[1];                               //always output b_r[1] and shift from higher index
+    always @(*) begin
+        case (count_r)
+            4'd0: begin
+                x1_out = x_r[5];  //see ppt P.4 for this implementation, 1 5 9 13 2 6 10 14 3 7 11 15 4 8 12 16
+                x2_out = 32'd0; //see ppt P.4 for this implementation
+                x3_out = x_r[9];  //see ppt P.4 for this implementation
+                x4_out = 32'd0; //see ppt P.4 for this implementation
+                x5_out = x_r[13];  //see ppt P.4 for this implementation
+                x6_out = 32'd0;
+            end
+            4'd1: begin
+                x1_out = x_r[5];  //see ppt P.4 for this implementation, 5 9 13 2 6 10 14 3 7 11 15 4 8 12 16 1
+                x2_out = x_r[12]; //see ppt P.4 for this implementation
+                x3_out = x_r[9];  //see ppt P.4 for this implementation
+                x4_out = x_r[8]; //see ppt P.4 for this implementation
+                x5_out = x_r[13];  //see ppt P.4 for this implementation
+                x6_out = x_r[4];
+                end
+            4'd2: begin
+                x1_out = x_r[5];  //see ppt P.4 for this implementation, 9 13 2 6 10 14 3 7 11 15 4 8 12 16 1 5
+                x2_out = x_r[12]; //see ppt P.4 for this implementation
+                x3_out = x_r[9];  //see ppt P.4 for this implementation
+                x4_out = x_r[8]; //see ppt P.4 for this implementation
+                x5_out = x_r[13];  //see ppt P.4 for this implementation
+                x6_out = x_r[4];
+                end
+            4'd3: begin
+                x1_out = x_r[5];  //see ppt P.4 for this implementation, 13 2 6 10 14 3 7 11 15 4 8 12 16 1 5 9
+                x2_out = x_r[12]; //see ppt P.4 for this implementation
+                x3_out = x_r[9];  //see ppt P.4 for this implementation
+                x4_out = x_r[8]; //see ppt P.4 for this implementation
+                x5_out = x_r[13];  //see ppt P.4 for this implementation
+                x6_out = x_r[4];
+                end
+            4'd4: begin
+                x1_out = x_r[5];  //see ppt P.4 for this implementation, 2 6 10 14 3 7 11 15 4 8 12 16 1 5 9 13
+                x2_out = x_r[13]; //see ppt P.4 for this implementation
+                x3_out = x_r[9];  //see ppt P.4 for this implementation
+                x4_out = 32'd0; //see ppt P.4 for this implementation
+                x5_out = x_r[14];  //see ppt P.4 for this implementation
+                x6_out = 32'd0;
+                end
+            4'd5: begin
+                x1_out = x_r[5];  //see ppt P.4 for this implementation, 6 10 14 3 7 11 15 4 8 12 16 1 5 9 13 2
+                x2_out = x_r[13]; //see ppt P.4 for this implementation
+                x3_out = x_r[9];  //see ppt P.4 for this implementation
+                x4_out = x_r[8]; //see ppt P.4 for this implementation
+                x5_out = x_r[14];  //see ppt P.4 for this implementation
+                x6_out = x_r[4];
+                end
+            4'd6: begin
+                x1_out = x_r[5];  //see ppt P.4 for this implementation, 10 14 3 7 11 15 4 8 12 16 1 5 9 13 2 6
+                x2_out = x_r[13]; //see ppt P.4 for this implementation
+                x3_out = x_r[9];  //see ppt P.4 for this implementation
+                x4_out = x_r[8]; //see ppt P.4 for this implementation
+                x5_out = x_r[14];  //see ppt P.4 for this implementation
+                x6_out = x_r[4];
+                end
+            4'd7: begin
+                x1_out = x_r[5];  //see ppt P.4 for this implementation, 14 3 7 11 15 4 8 12 16 1 5 9 13 2 6 10
+                x2_out = x_r[13]; //see ppt P.4 for this implementation
+                x3_out = x_r[9];  //see ppt P.4 for this implementation
+                x4_out = x_r[8]; //see ppt P.4 for this implementation
+                x5_out = 32'd0;  //see ppt P.4 for this implementation
+                x6_out = x_r[4];
+                end
+            4'd8: begin
+                x1_out = x_r[5];  //see ppt P.4 for this implementation, 3 7 11 15 4 8 12 16 1 5 9 13 2 6 10 14
+                x2_out = x_r[13]; //see ppt P.4 for this implementation
+                x3_out = x_r[10];  //see ppt P.4 for this implementation
+                x4_out = x_r[9]; //see ppt P.4 for this implementation
+                x5_out = x_r[14];  //see ppt P.4 for this implementation
+                x6_out = 32'd0;
+                end
+            4'd9: begin
+                x1_out = x_r[5];  //see ppt P.4 for this implementation, 7 11 15 4 8 12 16 1 5 9 13 2 6 10 14 3
+                x2_out = x_r[13]; //see ppt P.4 for this implementation
+                x3_out = x_r[10];  //see ppt P.4 for this implementation
+                x4_out = x_r[9]; //see ppt P.4 for this implementation
+                x5_out = x_r[14];  //see ppt P.4 for this implementation
+                x6_out = x_r[4];
+                end
+            4'd10: begin
+                x1_out = x_r[5];  //see ppt P.4 for this implementation, 11 15 4 8 12 16 1 5 9 13 2 6 10 14 3 7
+                x2_out = x_r[13]; //see ppt P.4 for this implementation
+                x3_out = x_r[10];  //see ppt P.4 for this implementation
+                x4_out = x_r[9]; //see ppt P.4 for this implementation
+                x5_out = x_r[14];  //see ppt P.4 for this implementation
+                x6_out = x_r[4];
+            end
+            4'd11: begin
+                x1_out = x_r[5];  //see ppt P.4 for this implementation, 15 4 8 12 16 1 5 9 13 2 6 10 14 3 7 11
+                x2_out = x_r[13]; //see ppt P.4 for this implementation
+                x3_out = 32'd0;  //see ppt P.4 for this implementation
+                x4_out = x_r[9]; //see ppt P.4 for this implementation
+                x5_out = 32'd0;  //see ppt P.4 for this implementation
+                x6_out = x_r[4];
+            end
+            4'd12: begin
+                x1_out = x_r[6];  //see ppt P.4 for this implementation, 4 8 12 16 1 5 9 13 2 6 10 14 3 7 11 15
+                x2_out = x_r[13]; //see ppt P.4 for this implementation
+                x3_out = x_r[10];  //see ppt P.4 for this implementation
+                x4_out = x_r[9]; //see ppt P.4 for this implementation
+                x5_out = x_r[14];  //see ppt P.4 for this implementation
+                x6_out = x_r[5];
+            end
+            4'd13: begin
+                x1_out = x_r[6];  //see ppt P.4 for this implementation, 8 12 16 1 5 9 13 2 6 10 14 3 7 11 15 4
+                x2_out = x_r[13]; //see ppt P.4 for this implementation
+                x3_out = x_r[10];  //see ppt P.4 for this implementation
+                x4_out = x_r[9]; //see ppt P.4 for this implementation
+                x5_out = x_r[14];  //see ppt P.4 for this implementation
+                x6_out = x_r[5];
+            end
+            4'd14: begin
+                x1_out = x_r[6];  //see ppt P.4 for this implementation, 12 16 1 5 9 13 2 6 10 14 3 7 11 15 4 8
+                x2_out = x_r[13]; //see ppt P.4 for this implementation
+                x3_out = x_r[10];  //see ppt P.4 for this implementation
+                x4_out = x_r[9]; //see ppt P.4 for this implementation
+                x5_out = x_r[14];  //see ppt P.4 for this implementation
+                x6_out = x_r[5];
+            end
+            4'd15: begin
+                x1_out = 32'd0;  //see ppt P.4 for this implementation, 16 1 5 9 13 2 6 10 14 3 7 11 15 4 8 12
+                x2_out = x_r[13]; //see ppt P.4 for this implementation
+                x3_out = 32'd0;  //see ppt P.4 for this implementation
+                x4_out = x_r[9]; //see ppt P.4 for this implementation
+                x5_out = 32'd0;  //see ppt P.4 for this implementation
+                x6_out = x_r[5];
+            end
+        endcase
+    end
     assign start_out = start_r;                              //To let GSIM know the out_valid can be pulled up
 
 endmodule
